6ccd132b1ecc67fc7be0c6744928a1da4474a145
Zymantas Katinas <kyssaz@gmail.com>
2020-05-26 14:11:52 +0300

    Updates

---
+++ precache-manifest.c26d780071d9e17c35e8d1d2cb001a97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02d67e81d5aca927e770f84dbf27e1d8",
+    "url": "/cats-dogs/index.html"
+  },
+  {
+    "revision": "026b5947555f4337c00d",
+    "url": "/cats-dogs/static/css/main.2085ea38.chunk.css"
+  },
+  {
+    "revision": "ed679927d26eb18548a4",
+    "url": "/cats-dogs/static/js/2.d3e1ad05.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cats-dogs/static/js/2.d3e1ad05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "026b5947555f4337c00d",
+    "url": "/cats-dogs/static/js/main.3e843bd2.chunk.js"
+  },
+  {
+    "revision": "f4a49668925ff9e2e10c",
+    "url": "/cats-dogs/static/js/runtime-main.9baf8f27.js"
+  }
+]);
